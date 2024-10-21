158f5aa3f03b8b8a26c29638bc2f736580fe351e
Kaustubh <kaustubhjaitapkar2004@gmail.com>
2024-10-22 00:37:39 +0530

    cors changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,10 +6,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(
+    cors({
+      origin: "https://med-bed1-hospital-frontend.vercel.app/",
+      methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+      credentials: true,
+      // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+    })
+  );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
