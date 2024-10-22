dc5082c9e6bedd8ef470e32f6e2e81b80946bdb8
Kaustubh <kaustubhjaitapkar2004@gmail.com>
2024-10-22 09:14:09 +0530

    changes

--- user-frontend/src/Pages/ReserveBed.jsx
+++ user-frontend/src/Pages/ReserveBed.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom'; // Import useNavigate from react-router-dom
-import Cookies from 'js-cookies';
 
 const getCookie = (cookieName) => {
   const name = cookieName + "=";
